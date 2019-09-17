d609d201ab5c2ded185b11e808dc5b6a67e07d12
unknown <commodari.a@gmail.com>
2019-09-16 22:45:22 -0400

    prepping for heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const startServer = async () => {
   const app = express();
 
   await require('./loaders')(app);
+
+  // Serve static assets if in production
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
   const port = process.env.PORT || 5000;
 
